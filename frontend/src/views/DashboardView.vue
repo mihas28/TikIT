4d396025ec2b0cca69b82d48c51859ca8933bc62
mihas28 <miha.skrabolje@gmail.com>
2025-04-20 10:44:13 +0200

    Urejanje domače strani - dashboarda

--- frontend/src/views/DashboardView.vue
+++ frontend/src/views/DashboardView.vue
@@ -1,19 +1,26 @@
 <script setup lang="ts">
 import { useRouter } from 'vue-router';
 import { useAuthStore } from '@/stores/authStore';
+import { onMounted } from 'vue';
 
 const router = useRouter();
 const authStore = useAuthStore();
 
-const logout = () => {
-    authStore.logout();
-    router.push('/login');
-};
+onMounted(async () => {
+  if (authStore.getUserRole === 'user') {
+    router.replace('/my-tickets');
+  }
+  else if (authStore.getUserRole === 'operator') {
+    router.replace('/assigned-tickets');
+  } else if (authStore.getUserRole === 'admin') {
+    router.replace('/all-tickets');
+  } else {
+    router.replace('/login');
+  }
+});
+
 </script>
 
 <template>
-    <div>
-        <h1>Dashboard</h1>
-        <button @click="logout">Odjava</button>
-    </div>
+
 </template>
